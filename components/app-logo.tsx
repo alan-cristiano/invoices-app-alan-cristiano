ad3cd9910715b844478a4891c00273334e621821
alan <alan_cd@hotmail.com>
2024-09-05 16:54:05 -0300

    refactor: refactor app-logo component

--- components/app-logo.tsx
+++ components/app-logo.tsx
@@ -4,9 +4,9 @@ import { ChartBarSquareIcon } from "@heroicons/react/24/outline";
 export default function AppLogo() {
     return (
         <div
-            className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+            className={`${lusitana.className} flex flex-col items-start leading-none text-white`}
         >
-            <ChartBarSquareIcon className="h-12 w-12" />
+            <ChartBarSquareIcon className="md:block hidden h-12 w-12" />
             <p className="text-[44px] ">InvApp</p>
         </div>
     );
